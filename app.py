4e337ff1e29406512cdd054af7b81ca82d8ec1d0
akanksha_23x07 <akanksha.purwar2000@gmail.com>
2025-07-01 07:11:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,16 +6,16 @@ import tensorflow as tf
 import pickle
 
 # Load the trained model
-model = tf.keras.models.load_model('sample_data/model.h5')
+model = tf.keras.models.load_model('model.h5')
 
 ## load encoder and scaler
-with open('sample_data/label_encoder_gender.pkl', 'rb') as file:
+with open('label_encoder_gender.pkl', 'rb') as file:
   label_encoder_gender= pickle.load(file)
 
-with open('sample_data/onehot_encoder_geo.pkl', 'rb') as file:
+with open('onehot_encoder_geo.pkl', 'rb') as file:
   onehot_encoder_geo= pickle.load(file)
 
-with open('sample_data/scaler.pkl', 'rb') as file:
+with open('scaler.pkl', 'rb') as file:
   scaler= pickle.load(file)
 
 
